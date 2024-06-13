b3a59024989ffc9a185197d8ff09473bf86a62fc
Colin503 <86229895+Colin503@users.noreply.github.com>
2024-06-13 14:39:33 +0200

    Update projet.h

--- projet.h
+++ projet.h
@@ -1,1 +1,14 @@
+//
+// Created by colin on 13/06/2024.
+//
 
+#ifndef S2_PROJETFINAL_PROJET_H
+#define S2_PROJETFINAL_PROJET_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+
+
+#endif //S2_PROJETFINAL_PROJET_H
