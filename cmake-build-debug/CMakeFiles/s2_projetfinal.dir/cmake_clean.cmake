671dcf7e709e1dc28d5c071ae31bb12abaa3625a
Colin503 <86229895+Colin503@users.noreply.github.com>
2024-06-13 14:42:19 +0200

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/s2_projetfinal.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/s2_projetfinal.dir/main.c.o"
+  "CMakeFiles/s2_projetfinal.dir/main.c.o.d"
+  "CMakeFiles/s2_projetfinal.dir/projet.c.o"
+  "CMakeFiles/s2_projetfinal.dir/projet.c.o.d"
+  "s2_projetfinal"
+  "s2_projetfinal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/s2_projetfinal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
