2798f74a173208752619c1a1de7e5e52e5f31289
Mvnech <manech.rbourgeo@gmail.com>
2024-06-14 16:36:26 +0200

    Update filemanager.c

--- filemanager.c
+++ filemanager.c
@@ -1,7 +1,19 @@
-//
-// Created by colin on 14/06/2024.
-//
-
-#include "person.h"
-#include "population.h"
 #include "filemanager.h"
+
+Population read_csv(const char* filename){
+    FILE* f = fopen(filename, "r");
+    int n=0;
+    char buffer[256];
+    while (fgets(buffer,256,f))
+        n++;
+    Person** tab= malloc(n* sizeof(Person*));
+    Population pop;
+    pop.nb_personne=0;
+    pop.tab_personne=tab;
+    rewind(f);
+    for(int i=0;i<n;i++){
+        Person* p=initPerson(fgets(buffer,256,f));
+        insert(&pop, i,p);
+    }
+    return pop;
+}
