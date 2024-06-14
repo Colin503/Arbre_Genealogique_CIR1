b8dba7a0c89f9a5874f6347d214a4c9d747dd23a
Mvnech <manech.rbourgeo@gmail.com>
2024-06-14 16:38:07 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,30 +1,14 @@
 #include "population.h"
 
 int main() {
-    Person* p1= initPerson("0,0,0,-,-,-,-");
-    Person* p= initPerson("17,39,13,Hodgkins,Florencia,28/12/1839,Herefordshire");
-    Person* p2= initPerson("13,0,0,Mccormick,Rosina,22/2/1815,Worcestershire");
-    Person* p3= initPerson("39,0,0,Hodgkins,Salvatore,23/9/1808,Herefordshire");
- printf("Id: %d\nId father: %d\nId mother: %d\nNom: %s\nPrénom: %s\nDate de naissance: %d/%d/%d\nVille de naissance: %s\n ",
-           p->id,p->father_id,p->mother_id,p->lastname,p->firstname,p->birthday,p->birthmonth,p->birthyear,p->birthzipcode);
     Population pop;
-    Person** tab= malloc(40* sizeof(Person));
-    pop.tab_personne=tab;
-    insert(&pop,0,p1);
-    insert(&pop,1,p);
-    insert(&pop,2,p2);
-    insert(&pop,3,p3);
+    pop = read_csv("../40.csv");
     linkPopulation(pop);
-    printf("%p\n",pop.tab_personne[0]->p_father);
-    printf("%p\n",pop.tab_personne[0]->p_mother);
-
-    printf("%p\n",pop.tab_personne[1]->p_mother);
-    printf("%p\n",pop.tab_personne[2]);
-    printf("%p\n",pop.tab_personne[2]->p_mother);
-    printf("%p\n",pop.tab_personne[2]->p_father);
-    printf("%p\n",pop.tab_personne[1]->p_father);
-    printf("%p\n",pop.tab_personne[3]);
-    printf("%p\n",pop.tab_personne[3]->p_mother);
-    printf("%p\n",pop.tab_personne[3]->p_father);
+    for(int i=0;i<pop.nb_personne;i++) {
+        printf("Adresse:%p Id: %d Id father: %d Id mother: %d Nom: %s Prénom: %s Date de naissance: %d/%d/%d Ville de naissance: %s Adresse mère:%p, Adresse père:%p\n ",
+               pop.tab_personne[i],pop.tab_personne[i]->id, pop.tab_personne[i]->father_id, pop.tab_personne[i]->mother_id,
+               pop.tab_personne[i]->lastname, pop.tab_personne[i]->firstname, pop.tab_personne[i]->birthday,
+               pop.tab_personne[i]->birthmonth, pop.tab_personne[i]->birthyear, pop.tab_personne[i]->birthzipcode, pop.tab_personne[i]->p_mother, pop.tab_personne[i]->p_father);
+    }
     return 0;
 }
