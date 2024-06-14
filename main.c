c2b951e41ab6a3fa17613578e83623fd19471058
Mvnech <manech.rbourgeo@gmail.com>
2024-06-14 16:48:39 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,11 @@
 #include "population.h"
 
 int main() {
-    Population pop;
+       Population pop;
     pop = read_csv("../40.csv");
     linkPopulation(pop);
-    for(int i=0;i<pop.nb_personne;i++) {
-        printf("Adresse:%p Id: %d Id father: %d Id mother: %d Nom: %s Prénom: %s Date de naissance: %d/%d/%d Ville de naissance: %s Adresse mère:%p, Adresse père:%p\n ",
-               pop.tab_personne[i],pop.tab_personne[i]->id, pop.tab_personne[i]->father_id, pop.tab_personne[i]->mother_id,
-               pop.tab_personne[i]->lastname, pop.tab_personne[i]->firstname, pop.tab_personne[i]->birthday,
-               pop.tab_personne[i]->birthmonth, pop.tab_personne[i]->birthyear, pop.tab_personne[i]->birthzipcode, pop.tab_personne[i]->p_mother, pop.tab_personne[i]->p_father);
-    }
+    Person** tab = fratrie(pop, pop.tab_personne[26]);
+    for(int i=0;i<3;i++)
+        printf("%d\n",tab[i]->id);
     return 0;
 }
