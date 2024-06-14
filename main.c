b7efe54100226f7aa0f4ff96ac8b84d556191851
Colin503 <86229895+Colin503@users.noreply.github.com>
2024-06-14 11:24:15 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,20 @@
 #include "population.h"
 
 int main() {
+    Person *p = initPerson("1,7,24,Mccormick,Rozanne,23/10/1807,Staffordshire");
+    Person** tab_personne= malloc(40*sizeof(Person));
+    //tab_personne[0]=p;
+    Population pop1;
+    pop1.tab_personne=tab_personne;
+    pop1.nb_personne=0;
     Person* p1= initPerson("0,0,0,-,-,-,-");
-    Person* p= initPerson("17,39,13,Hodgkins,Florencia,28/12/1839,Herefordshire");
     Person* p2= initPerson("17,39,13,Hodgkins,Florencia,28/12/1839,Herefordshire");
     Person* p3= initPerson("17,39,13,Hodgkins,Florencia,28/12/1839,Herefordshire");
-    printf("Id: %d\nId father: %d\nId mother: %d\nNom: %s\nPrénom: %s\nDate de naissance: %d/%d/%d\nVille de naissance: %s\n ",
-           p->id,p->father_id,p->mother_id,p->lastname,p->firstname,p->birthday,p->birthmonth,p->birthyear,p->birthzipcode);
-    Population pop;
+    Population* pop=&pop1;
     insert(pop,0,p1);
     insert(pop,1,p);
     insert(pop,2,p2);
     insert(pop,3,p3);
+    printf("%d\n%d\n%d\n",pop1.tab_personne[0]->id,pop1.tab_personne[1]->id,pop1.tab_personne[2]->id);
     return 0;
 }
