0be334a1112a1559b60b015bec184037bbbfe04d
Mvnech <manech.rbourgeo@gmail.com>
2024-06-14 09:54:48 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main() {
     Person* p= initPerson("1,7,24,Mccormick,Rozanne,23/10/1807,Staffordshire");
-    printf("Id: %d\nId father: %d\nId mother: %d\nNom: %s\nPrénom: %s\n Date de naissance: %d/%d/%d\n Ville de naissance: %s\n ",
+    printf("Id: %d\nId father: %d\nId mother: %d\nNom: %s\nPrénom: %s\nDate de naissance: %d/%d/%d\nVille de naissance: %s\n ",
            p->id,p->father_id,p->mother_id,p->lastname,p->firstname,p->birthday,p->birthmonth,p->birthyear,p->birthzipcode);
     return 0;
 }
