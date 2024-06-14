8975ac1619ae60c93d2c1c1ac06e669f679429bd
Mvnech <manech.rbourgeo@gmail.com>
2024-06-14 13:58:44 +0200

    Update population.c

--- population.c
+++ population.c
@@ -9,3 +9,18 @@ void insert(Population* p, int key, Person* data ) {
     p->tab_personne[key]=data;
     p->nb_personne++;
 }
+
+void linkPopulation(Population p){
+    int nb_pers=p.nb_personne;
+    Person** tab_id= malloc(nb_pers* sizeof(Person*));
+    for(int i=0;i<nb_pers;i++) {
+        tab_id[i] = NULL;
+        tab_id[p.tab_personne[i]->id] = p.tab_personne[i];
+    }
+    for(int i=0;i<nb_pers;i++){
+        Person* pers=p.tab_personne[i];
+        pers->p_mother=tab_id[pers->mother_id];
+        pers->p_father=tab_id[pers->father_id];
+    }
+
+}
