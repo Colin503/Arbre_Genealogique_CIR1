8220d4f615bbd9c748cfd3ed2fdf574539f0fc0b
Colin503 <86229895+Colin503@users.noreply.github.com>
2024-06-14 11:32:53 +0200

    Add files via upload

---
+++ filemanager.c
@@ -0,0 +1,7 @@
+//
+// Created by colin on 14/06/2024.
+//
+
+#include "person.h"
+#include "population.h"
+#include "filemanager.h"
---
+++ filemanager.h
@@ -0,0 +1,13 @@
+//
+// Created by colin on 14/06/2024.
+//
+
+#ifndef S2_PROJETFINAL_FILEMANAGER_H
+#define S2_PROJETFINAL_FILEMANAGER_H
+#include "person.h"
+#include "population.h"
+
+
+
+#endif //S2_PROJETFINAL_FILEMANAGER_H
+
