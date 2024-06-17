2c6524e7f0d720c0c975be0dd4530a02f292dc1c
Colin503 <86229895+Colin503@users.noreply.github.com>
2024-06-17 09:25:07 +0200

    Update advanced.c

--- advanced.c
+++ advanced.c
@@ -21,7 +21,14 @@ Person** fratrie(Population p,Person* humain) {
 Person** ancetre(Population p,Person* humain) {
     Person** tab_ancetre= malloc(7* sizeof(Person*)); //7 cases car on se limites à 2 générations pour l'instant
     tab_ancetre[0]=humain;
-    for (int i=0;i<p.nb_personne;i++) {
+    tab_ancetre[1]=humain->p_father;
+    tab_ancetre[2]=humain->p_mother;
+    tab_ancetre[3]=humain->p_father->p_father;
+    tab_ancetre[4]=humain->p_father->p_mother;
+    tab_ancetre[5]=humain->p_mother->p_father;
+    tab_ancetre[6]=humain->p_mother->p_mother;
+
+    /*for (int i=0;i<p.nb_personne;i++) {
         if (p.tab_personne[i]->id == humain->father_id)  //id --> pere humain
             tab_ancetre[1] = p.tab_personne[i];
         if (p.tab_personne[i]->id == humain->mother_id) //id --> mere humain
@@ -38,6 +45,6 @@ Person** ancetre(Population p,Person* humain) {
             if (p.tab_personne[i]->id == tab_ancetre[2]->mother_id) //id --> mere de la mere humain
                 tab_ancetre[6] = p.tab_personne[i];
         }
-    }
+    }*/
     return tab_ancetre;
 }
