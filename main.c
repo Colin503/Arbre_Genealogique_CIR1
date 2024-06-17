ac653ea3fcd80dfb10e20e416d0cb2e93a276e6f
Colin503 <86229895+Colin503@users.noreply.github.com>
2024-06-17 09:28:02 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,9 @@
-#include "population.h"
-
 int main() {
-       Population pop;
-    pop = read_csv("../40.csv");
+    Population pop;
+    pop = read_csv("../resources/40.csv");
     linkPopulation(pop);
-    Person** tab = fratrie(pop, pop.tab_personne[26]);
-    for(int i=0;i<3;i++)
+    Person** tab = ancetre(pop, pop.tab_personne[22]);
+    for(int i=0;i<5;i++)
         printf("%d\n",tab[i]->id);
     return 0;
 }
