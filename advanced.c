95ef757c05af712dd994e73b898b5df7b730385b
Colin503 <86229895+Colin503@users.noreply.github.com>
2024-06-17 09:41:41 +0200

    Update advanced.c

--- advanced.c
+++ advanced.c
@@ -18,33 +18,15 @@ Person** fratrie(Population p,Person* humain) {
     return tab_fratrie;
 }
 
-Person** ancetre(Population p,Person* humain) {
+Person** ancestorsPersons(Population t, Person* p) {
     Person** tab_ancetre= malloc(7* sizeof(Person*)); //7 cases car on se limites à 2 générations pour l'instant
-    tab_ancetre[0]=humain;
-    tab_ancetre[1]=humain->p_father;
-    tab_ancetre[2]=humain->p_mother;
-    tab_ancetre[3]=humain->p_father->p_father;
-    tab_ancetre[4]=humain->p_father->p_mother;
-    tab_ancetre[5]=humain->p_mother->p_father;
-    tab_ancetre[6]=humain->p_mother->p_mother;
-
-    /*for (int i=0;i<p.nb_personne;i++) {
-        if (p.tab_personne[i]->id == humain->father_id)  //id --> pere humain
-            tab_ancetre[1] = p.tab_personne[i];
-        if (p.tab_personne[i]->id == humain->mother_id) //id --> mere humain
-            tab_ancetre[2] = p.tab_personne[i];
-        if (tab_ancetre[1]!=NULL) {
-            if (p.tab_personne[i]->id == tab_ancetre[1]->father_id)  //id --> pere du pere humain
-                tab_ancetre[3] = p.tab_personne[i];
-            if (p.tab_personne[i]->id == tab_ancetre[1]->mother_id) //id --> mere du pere humain
-                tab_ancetre[4] = p.tab_personne[i];
-        }
-        if (tab_ancetre[2]!=NULL) {
-            if (p.tab_personne[i]->id == tab_ancetre[2]->father_id) // id --> pere de la mere humain
-                tab_ancetre[5] = p.tab_personne[i];
-            if (p.tab_personne[i]->id == tab_ancetre[2]->mother_id) //id --> mere de la mere humain
-                tab_ancetre[6] = p.tab_personne[i];
-        }
-    }*/
+    tab_ancetre[0]=p;
+    tab_ancetre[1]=p->p_father;
+    tab_ancetre[2]=p->p_mother;
+    tab_ancetre[3]=p->p_father->p_father;
+    tab_ancetre[4]=p->p_father->p_mother;
+    tab_ancetre[5]=p->p_mother->p_father;
+    tab_ancetre[6]=p->p_mother->p_mother;
+    
     return tab_ancetre;
 }
