d2566f6af857eca4478f7e516b83fb07f69bb99c
Colin503 <86229895+Colin503@users.noreply.github.com>
2024-06-18 16:13:43 +0200

    Update advanced.h

--- advanced.h
+++ advanced.h
@@ -11,6 +11,7 @@ Fratrie fratrie(Population p,Person* humain);
 Person** ancestorsPersons(Population p,Person* humain);
 void ancestors_debug(Person** tab,const char* filen);
 void fratrie_debug(Person** tab,const char* filen);
+Person* oldestancertor(Population t,Person* p);
 
 
 #endif //S2_PROJETFINAL_ADVANCED_H
