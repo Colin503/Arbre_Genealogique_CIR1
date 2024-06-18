21e16b8b64b5782666a2b096171070cab5e144ed
Mvnech <manech.rbourgeo@gmail.com>
2024-06-18 17:33:42 +0200

    Update htmlexport.h

--- htmlexport.h
+++ htmlexport.h
@@ -3,9 +3,12 @@
 #include "advanced.h"
 
 int titreHTMLPerson(char* buffer, Person* p);
+int fichePath_page(char* buffer, Person* p);
 int fichePath(char* buffer, Person* p);
 void exportAncestorHTML(const Population t,Person* p, char* path);
+void exportficheHTML(const Population t,Person* p, char* path);
 int printAncestorsToHTML(char* buffer, const Population t, Person* p);
 void exportFratrieHTML(const Population t,Person* p, char* path);
 int printFratrieToHTML(char* buffer, const Population t, Person* p);
+int contenu_fiche(char* buffer, const Population t, Person* p);
 #endif //PROJET_FIN_HTMLEXPORT_H
