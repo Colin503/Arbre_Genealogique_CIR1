f4ef0fcb02f3ba9892244a98b5741ea32907369d
Colin503 <86229895+Colin503@users.noreply.github.com>
2024-06-18 18:10:38 +0200

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,35 +1,37 @@
-//
-// Created by colin on 17/06/2024.
-//
-
 #include "menu.h"
 
 void menu(Population t) {
     char temp;
     char path[30];
     int id;
+
     printf("Hey, que souhaitez vous rechercher ?\n\n(1) La Fratrie d'une personne\n(2) Les ancetres d'une personne sur 4 générations\n(3) Le plus lointain ancetre d'une personne\n");
     scanf("%c",&temp);
     switch (temp) {
         case '1':
             printf("Entrez l'id de la personne que vous souhaitez rechercher --> ");
-            scanf("%d",&id);
-            fichePath(path,t.tab_personne[id]);
-            exportFratrieHTML(t,t.tab_personne[id],path);
+            scanf("%d", &id);
+            fichePath(path, t.tab_personne[id]);
+            exportFratrieHTML(t, t.tab_personne[id], path);
             break;
         case '2':
             printf("Entrez l'id de la personne que vous souhaitez rechercher --> ");
-            scanf("%d",&id);
-            fichePath(path,t.tab_personne[id]);
-            exportAncestorHTML(t,t.tab_personne[id],path);
+            scanf("%d", &id);
+            fichePath(path, t.tab_personne[id]);
+            exportAncestorHTML(t, t.tab_personne[id], path);
             break;
         case '3':
             printf("Entrez l'id de la personne que vous souhaitez rechercher --> ");
-            scanf("%d",&id);
-            Person* old=oldestancertor(t,t.tab_personne[id]);
-            printf("Nom : %s\nPrenom : %s\nDate de naissance : %d/%d/%d\nVille de naissance : %s\n",old->lastname,old->firstname,old->birthday,old->birthmonth,old->birthyear,old->birthzipcode);
+            scanf("%d", &id);
+            Person *old = oldestancertor(t, t.tab_personne[id]);
+            printf("Nom : %s\nPrenom : %s\nDate de naissance : %d/%d/%d\nVille de naissance : %s\n", old->lastname,
+                   old->firstname, old->birthday, old->birthmonth, old->birthyear, old->birthzipcode);
             break;
-    }
+        default:
+            printf("Choix invalide. Fin du programme.\n");
+            break;
+            }
+
 }
 
 void affichage_tableau(Person** tab) {
