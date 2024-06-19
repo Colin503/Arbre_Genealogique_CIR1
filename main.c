cb6a3a4de17f30f19ac720af69fe6fafe9165084
Mvnech <manech.rbourgeo@gmail.com>
2024-06-19 11:30:28 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,15 @@
 #include "menu.h"
+
 void free_population(Population pop) {
     for (int i=0; i<pop.nb_personne; i++)
         free(pop.tab_personne[i]);
     free(pop.tab_personne);
 }
+
 int main() {
-    Population pop;
-    char path_p[30];
-    pop = read_csv("../10000.csv");
+    Population pop = read_csv("../resources/10000.csv");
     linkPopulation(pop);
-    fichePath_page(path_p,pop.tab_personne[4374]);
-    exportAncestorHTML(pop,pop.tab_personne[4374],path_p);
-    //exportFratrieHTML(pop,pop.tab_personne[9999],path_p);
+    menu(pop);
     free_population(pop);
     return 0;
 }
