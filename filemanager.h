59aaa9e8d0dad90f6e1363dc4ffdbbccf23e9834
Mvnech <manech.rbourgeo@gmail.com>
2024-06-19 11:44:16 +0200

    Update filemanager.h

--- filemanager.h
+++ filemanager.h
@@ -1,9 +1,9 @@
-#ifndef PROJET_FIN_FILEMANAGER_H
-#define PROJET_FIN_FILEMANAGER_H
+#ifndef PROJETCIR1_RENNES_GROUPE4_FILEMANAGER_H
+#define PROJETCIR1_RENNES_GROUPE4_FILEMANAGER_H
 
 #include "population.h"
 
 Population read_csv(const char* filename);
 Population write_csv(Population pop, const char* filename);
 
-#endif //PROJET_FIN_FILEMANAGER_H
+#endif //PROJETCIR1_RENNES_GROUPE4_FILEMANAGER_H
