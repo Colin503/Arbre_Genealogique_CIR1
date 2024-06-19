25cfba56bf03dc61a53f787ef1a47ef5c5e96115
Mvnech <manech.rbourgeo@gmail.com>
2024-06-19 11:33:46 +0200

    Update htmlexport.h

--- htmlexport.h
+++ htmlexport.h
@@ -1,16 +1,21 @@
 #ifndef PROJET_FIN_HTMLEXPORT_H
 #define PROJET_FIN_HTMLEXPORT_H
-#include "advanced.h"
+
+//Nombre maximum de carctères contenus dans le chemin d'accès
 #define TAILLE_CHEMIN_ACCES 100
-#define TAILLE_MAX 100000
+//Nombre maximum de caractères pour le corps de la page HTML
+#define TAILLE_MAX 100000 
+
+#include "advanced.h"
+
 
 int titreHTMLPerson(char* buffer, Person* p);
 int fichePath_page(char* buffer, Person* p);
 int fichePath(char* buffer, Person* p);
-void exportAncestorHTML(const Population t,Person* p, char* path);
-void exportficheHTML(const Population t,Person* p, char* path);
-int printAncestorsToHTML(char* buffer, const Population t, Person* p);
+void exportAncestorHTML(Person* p, char* path);
+void exportficheHTML(Person* p, char* path);
+int printAncestorsToHTML(char* buffer, Person* p);
 void exportFratrieHTML(const Population t,Person* p, char* path);
 int printFratrieToHTML(char* buffer, const Population t, Person* p);
-int contenu_fiche(char* buffer, const Population t, Person* p);
+int contenu_fiche(char* buffer, Person* p);
 #endif //PROJET_FIN_HTMLEXPORT_H
