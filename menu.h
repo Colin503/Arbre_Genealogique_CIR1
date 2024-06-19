5b66834b1509e80cb0a1820776e61a00d3030b07
Mvnech <manech.rbourgeo@gmail.com>
2024-06-19 11:14:48 +0200

    Update menu.h

--- menu.h
+++ menu.h
@@ -1,14 +1,9 @@
-//
-// Created by colin on 17/06/2024.
-//
-
-#ifndef S2_PROJETFINAL_MENU_H
-#define S2_PROJETFINAL_MENU_H
+#ifndef PROJET_FIN_MENU_H
+#define PROJET_FIN_MENU_H
 
 #include "htmlexport.h"
-
 void menu(Population t);
 void affichage_tableau(Person** tab);
 void affichage_fratrie(Fratrie f);
 
-#endif //S2_PROJETFINAL_MENU_H
+#endif //PROJET_FIN_MENU_H
